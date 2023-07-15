242cad5836bbb8941b6df77675cea3e11b0bbcfd
Alexey Chizhevskiy <aleksei.chizhevskii@gmail.com>
2023-07-15 21:13:34 +0500

    initial_commit

---
+++ articles/admin.py
@@ -0,0 +1,30 @@
+from django.contrib import admin
+from django.core.exceptions import ValidationError
+from django.forms import BaseInlineFormSet
+
+from .models import Article, Scope
+
+
+class ScopeInlineFormset(BaseInlineFormSet):
+    def clean(self):
+        main_tag_counter = 0
+        for form in self.forms:
+            if form.cleaned_data.get('is_main'):
+                main_tag_counter += 1
+        if main_tag_counter < 1:
+            raise ValidationError('Должен быть выбран основной тэг/раздел')
+        if main_tag_counter > 1:
+            raise ValidationError('Основной Тэг/раздел должен быть один')
+        return super().clean()
+            
+
+class ScopeInline(admin.TabularInline):
+    model = Scope
+    formset = ScopeInlineFormset
+    extra = 3
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    inlines = [ScopeInline]
+
+
---
+++ articles/views.py
@@ -0,0 +1,35 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+from articles.models import Article, Tag
+
+
+def articles_list(request):
+    template = 'articles/news.html'
+    articles = Article.objects.all()
+    context = {'object_list': articles}
+
+    # используйте этот параметр для упорядочивания результатов
+    # https://docs.djangoproject.com/en/3.1/ref/models/querysets/#django.db.models.query.QuerySet.order_by
+    ordering = '-published_at'
+
+    return render(request, template, context)
+
+
+def add_tags(request):
+    tag_list = [
+        'Наука',
+        'Здоровье',
+        'Культура',
+        'Город',
+        'Космос',
+
+    ]
+
+    for tag in tag_list:
+        Tag.objects.create(name=tag)
+
+    tag_str = ', '.join(tag_list)
+
+    return HttpResponse(f'Успешно добавлен в БД следующий список - {tag_str}.')
